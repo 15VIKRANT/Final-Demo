d42f018a39e976746120205874aaae65553b323f
15VIKRANT <vvraut010@gmail.com>
2025-02-12 19:53:55 +0530

    added-files-successfully

---
+++ src/App.js
@@ -0,0 +1,36 @@
+function App() {
+  return (
+    <div className="App" style={{display:"flex"}}>
+      <div style={{border:"1px solid red"}}>
+     <h1>---------------</h1>
+     <h2>Hello Boss</h2>
+     <h3>Hello Boss</h3>
+     <h4>Hello Boss</h4>
+     <h5>Hello Boss</h5>
+     <h6>Hello Boss</h6>
+     <h6>Hello Boss</h6>
+     <h5>Hello Boss</h5>
+     <h4>Hello Boss</h4>
+     <h3>Hello Boss</h3>
+     <h2>Hello Boss</h2>
+     <h1>---------------</h1>
+     </div>
+     <div style={{border:"1px solid red"}}>
+     <h1 style={{textAlign:"end"}} >---------------</h1>
+     <h2 style={{textAlign:"end"}} >ssoB olleH</h2>
+     <h3 style={{textAlign:"end"}} >ssoB olleH</h3>
+     <h4 style={{textAlign:"end"}} >ssoB olleH</h4>
+     <h5 style={{textAlign:"end"}} >ssoB olleH</h5>
+     <h6 style={{textAlign:"end"}}>ssoB olleH</h6>
+     <h6 style={{textAlign:"end"}} >ssoB olleH</h6>
+     <h5 style={{textAlign:"end"}} >ssoB olleH</h5>
+     <h4 style={{textAlign:"end"}} >ssoB olleH</h4>
+     <h3 style={{textAlign:"end"}} >ssoB olleH</h3>
+     <h2 style={{textAlign:"end"}} >ssoB olleH</h2>
+     <h1 style={{textAlign:"end"}} >---------------</h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
