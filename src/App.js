f0211b5282e8ba51a0a60dd0b1c21b7febbf9102
15VIKRANT <vvraut010@gmail.com>
2025-02-12 20:14:22 +0530

    Hello-world

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 function App() {
   return (
     <div className="App" style={{display:"flex"}}>
-      <div style={{border:"1px solid red"}}>
-     <h1>---------------</h1>
-     <h2>This is main branch</h2>
-     <h2>This is Branch Master</h2>
-     <h3>Hello Boss</h3>
-     <h4>Hello Boss</h4>
-     <h5>Hello Boss</h5>
-     <h6>Hello Boss</h6>
-     <h6>Hello Boss</h6>
-     <h5>Hello Boss</h5>
-     <h4>Hello Boss</h4>
-     <h3>Hello Boss</h3>
-     <h2>Hello Boss</h2>
-     <h1>---------------</h1>
-     </div>
-     <div style={{border:"1px solid red"}}>
-     <h1 style={{textAlign:"end"}} >---------------</h1>
-     <h2 style={{textAlign:"end"}} >ssoB olleH</h2>
-     <h3 style={{textAlign:"end"}} >ssoB olleH</h3>
-     <h4 style={{textAlign:"end"}} >ssoB olleH</h4>
-     <h5 style={{textAlign:"end"}} >ssoB olleH</h5>
-     <h6 style={{textAlign:"end"}}>ssoB olleH</h6>
-     <h6 style={{textAlign:"end"}} >ssoB olleH</h6>
-     <h5 style={{textAlign:"end"}} >ssoB olleH</h5>
-     <h4 style={{textAlign:"end"}} >ssoB olleH</h4>
-     <h3 style={{textAlign:"end"}} >ssoB olleH</h3>
-     <h2 style={{textAlign:"end"}} >ssoB olleH</h2>
-     <h1 style={{textAlign:"end"}} >---------------</h1>
-     </div>
+     
+     <h1>This is Feature</h1>
+    
+    
+     <a>Hello</a>
+    
     </div>
   );
 }
